2d3ecc62aa5f9ab8f09aeedaa2ab5be845f1b807
Jasjyot <jasjyot98@gmail.com>
2018-01-24 19:59:49 +0530

    Update and rename Arrays: Left Rotation.cpp to Arrays:Left_Rotation.cpp

--- Arrays:Left_Rotation.cpp
+++ Arrays:Left_Rotation.cpp
@@ -1,7 +1,7 @@
 /*
-A left rotation operation on an array of size  shifts each of the array's elements  unit to the left. For example, if left rotations are performed on array , then the array would become .
-
-Given an array of  integers and a number, , perform  left rotations on the array. Then print the updated array as a single line of space-separated integers.
+A left rotation operation on an array of size  shifts each of the array's elements  unit to the left. 
+Given an array of  integers and a number, , perform  left rotations on the array. 
+Then print the updated array as a single line of space-separated integers.
 Source: Hackerrank
 */
 
