2adb265b01b35386b1cd956e9ad89e65f6e960b0
Jasjyot <jasjyot98@gmail.com>
2018-01-24 19:58:39 +0530

    Create Arrays: Left Rotation.cpp

---
+++ Arrays: Left Rotation.cpp
@@ -0,0 +1,37 @@
+/*
+A left rotation operation on an array of size  shifts each of the array's elements  unit to the left. For example, if left rotations are performed on array , then the array would become .
+
+Given an array of  integers and a number, , perform  left rotations on the array. Then print the updated array as a single line of space-separated integers.
+Source: Hackerrank
+*/
+
+
+#include<iostream>
+using namespace std;
+int main()    
+{
+    int n,d;
+    cin>>n>>d;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int a[d];
+    for(int k=0;k<d;k++)
+    {
+        a[k]=arr[k];
+    }
+    for(int j=0;j<n-d;j++)
+    {
+    arr[j]=arr[j+d];
+    }
+    for(int k=0;k<d;k++){
+        arr[n-d+k]=a[k];
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
