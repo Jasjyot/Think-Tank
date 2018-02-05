796261b48c0fd886a5b5f065a8aa385f02e09adf
Jasjyot <jasjyot98@gmail.com>
2018-02-05 11:12:27 +0530

    Create decimal_to_binary.cpp

---
+++ decimal_to_binary.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int convert(int n);
+int convert(int n)
+{   int count=0,sum=0;
+    while(n)
+    {
+        int remainder=n%10;
+        sum=sum+(remainder*pow(2,count));
+        count++;
+        n=n/10;
+    }
+    return sum;
+}
+int main() {
+    int testCase;
+    cin>>testCase;
+    while(testCase)
+    {
+        int n;
+        cin>>n;
+        cout<<convert(n)<<endl;
+        testCase--;
+    }
+    return 0;
+}
