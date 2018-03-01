f30a468c96266c24221766d30306c38250116a67
Jasjyot Singh <jasjyot98@gmail.com>
2018-03-01 15:22:35 +0530

    Create unknown_no_inputs.cpp

---
+++ unknown_no_inputs.cpp
@@ -0,0 +1,14 @@
+
+// taking input of unknown number of inputs :)
+
+
+#include <iostream>
+using namespace std;
+int main() {
+    int no;
+    
+    while(scanf("%d",&no)!=EOF){
+        cout<<no*no<<endl;
+    }
+    return 0;
+}
