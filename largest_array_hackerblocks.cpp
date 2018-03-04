a9c140161362d9a0825ee5b567b1236f3e45b49e
Jasjyot Singh <jasjyot98@gmail.com>
2018-03-05 01:38:36 +0530

    Create largest_array_hackerblocks.cpp

---
+++ largest_array_hackerblocks.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n,max;
+    cin>>n;
+    int arr[n];
+    
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    if(n>0){
+        max=arr[0];
+    }
+    
+    for(int i=0;i<n;i++){
+        
+        if(max<arr[i]){
+            max=arr[i];            
+        }
+        
+    }
+    cout<<max;
+    
+	return 0;
+}
